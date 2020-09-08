9bf69f6e6bf461c67180da6eb63d9765f6e8d1d3
eyalgreenblatt <eyalgreenblatt@gmail.com>
2020-09-08 13:06:42 +0300

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,13 +1,13 @@
 node default {
-  file { '/root/README':
-    ensure => file,
-    content => 'This is a readme change by eyal greenblatt',
-    owner   => 'root',
-  }
+
 }
 
 node 'master.dom37.lab.forescout.com' {
   include role::master_server
+    file { '/root/README':
+    ensure => file,
+    content => $fqdn,
+  }
 }
 
 node /^web/ { 
